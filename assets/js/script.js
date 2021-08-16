feb1008636e096854141c7c9146ee319b8a560f8
Markp1312 <mpg.peeters3@gmail.com>
2021-08-16 15:00:56 +0000

    added the function listenforclick

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,23 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+
+
+function listenclick(){
+
+/* Event listeners that listen for click on the button selected by user */
+
 rock_div.addEventListener("click", function () {
-    
+
 })
 
 paper_div.addEventListener("click", function () {
-   
+
 })
 
 scissors_div.addEventListener("click", function () {
-    
+
 })
+}
+
+listenclick();
