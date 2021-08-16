1b123cd2788fe2e13c87db0732faf6a64a3ee940
Markp1312 <mpg.peeters3@gmail.com>
2021-08-16 14:47:09 +0000

    Added basic variables JS

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let userScore = 0;
+let computerScore = 0;
+const playerScore_span = document.getElementById("playerscore-span")
+const compScore_span = document.getElementById("computerscore-span")
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
