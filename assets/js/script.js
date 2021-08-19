06dccf560b2c4215f64baac95707c509e379cebd
Markp1312 <mpg.peeters3@gmail.com>
2021-08-19 05:42:51 +0000

    Created the function playerChoice

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,27 +4,30 @@ let userScore = 0;
 let computerScore = 0;
 const playerScore_span = document.getElementById("playerscore-span")
 const compScore_span = document.getElementById("computerscore-span")
-const rock_div = document.getElementById("r");
-const paper_div = document.getElementById("p");
-const scissors_div = document.getElementById("s");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
 
-
-
-function listenclick(){
-
-/* Event listeners that listen for click on the button selected by user */
-
-rock_div.addEventListener("click", function () {
-
-})
-
-paper_div.addEventListener("click", function () {
-
-})
-
-scissors_div.addEventListener("click", function () {
-
-})
+function play(playerChoice) {
+    console.log("pooop" + playerChoice)
 }
 
-listenclick();
+function general() {
+
+    /* Event listeners that listen for click on the button selected by user */
+
+    rock_div.addEventListener("click", function () {
+        play("rock");
+
+    })
+
+    paper_div.addEventListener("click", function () {
+        play("paper");
+    })
+
+    scissors_div.addEventListener("click", function () {
+        play ("scissors");
+    })
+}
+
+general ();
